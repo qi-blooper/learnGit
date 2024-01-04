ae5e2ff80f88f8ee94e87a5c89918954f138c9bb
xiaohq <1447702103@qq.com>
2024-01-04 15:05:33 +0800

    RL_change

--- RL.py
+++ RL.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from AcousticPara import Coord
 
 def compute_angle(coordS,coordR):
     x = (coordR.lon - coordS.lon) * (111 * math.cos(coordS.lat / 180 * math.pi))
@@ -13,21 +14,40 @@ def compute_angle(coordS,coordR):
         azi = math.atan(x / y) + math.pi
     # if azi < 0:
     #     azi = azi + 2 * math.pi
-    return azi, R
+    # return azi, R
 
 
-s = (132.2278, 22.69225)
-r = [(132.1153, 22.818918),
-     (132.1153, 22.782024),
-     (132.25012, 22.818918),
-     (132.25012, 22.782024)]
+s = Coord(132.2278, 22.69225)
+p = [Coord(132.1153, 22.818918),
+     Coord(132.1153, 22.782024),
+     Coord(132.25012, 22.818918),
+     Coord(132.25012, 22.782024)]
 
 
 
 azi = []
 for i in range(4):
-    azi1, R1 = compute_angle(s, r[i])
+    azi1, R1 = compute_angle(s, p[i])
     azi1 = azi1 * 180.0 / np.pi
     azi.append(azi1)
 
+max1=max(azi)
+min1 = min(azi)
 print(azi)
+
+test = []
+for i in range(4):
+    azi1, R1 = compute_angle(p[i], s)
+    azi1 = azi1 * 180.0 / np.pi
+    test.append(azi1)
+
+print(test)
+
+'''
+[-39.331095353961594, -49.141952354017725, 9.233704829874934, 12.91893337497655]
+[140.69494926913902, 130.87666309630018, 189.22529352893423, 192.9107375445085]
+
+
+[320.6689046460384, 310.8580476459823, 9.233704829874934, 12.91893337497655]
+[140.69494926913902, 130.87666309630018, 189.22529352893423, 192.9107375445085]
+'''
